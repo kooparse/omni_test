e551009f5c5c127aa7e28f3bbdc1e7c8ea8faf09
Alexandre Chêne <kooparse@protonmail.com>
2018-08-13 17:59:13 +0200

    Test

---
+++ Assets/Scripts/generator/MapGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+
+    public int noiseWidth;
+    public int noiseHeight;
+    public float noiseScale;
+    public bool autoUpdate;
+
+    // Use this for initialization
+    public void GenerateMap()
+    {
+        float[,] noiseMap = Noise.GenerateNoiseMap(noiseWidth, noiseHeight, noiseScale);
+        noiseMap.
+
+        MapDisplay display = FindObjectOfType<MapDisplay>();
+        display.drawNoiseMap(noiseMap);
+    }
+
+}
---
+++ Assets/Scripts/generator/Noise.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Noise {
+
+	public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, float scale = 0.0001f) {
+		float [,] noiseMap = new float[mapWidth, mapHeight];
+
+        for (int x = 0; x < mapWidth; x++)
+        {
+            for (int y = 0; y < mapHeight; y++)
+            {
+				float sampleX = x / scale;
+				float sampleY = y / scale;
+				float perlinValue = Mathf.PerlinNoise(sampleX, sampleY);
+
+				noiseMap[x, y] = perlinValue;
+            }
+        }
+
+		return noiseMap;
+	}
+}
